8497671ac19028dea8554547c1691e3bc6b5f751
Srikanth Yalavarthi <10422671+srikanthy@users.noreply.github.com>
2019-10-01 17:34:00 +0530

    updated membench.c

--- membench.c
+++ membench.c
@@ -3,20 +3,15 @@
 #include <stdlib.h>
 #include <string.h>
 
-/* define cache sizes in kB */
-#ifndef L1_CACHE
-#define L1_CACHE 32
-#endif
+#define KB_CACHE_MIN 1
+#define KB_CACHE_MAX 1024
 
-#ifndef L2_CACHE
-#define L2_CACHE 256
-#endif
+#define CACHE_MIN (KB_CACHE_MIN * 1024)
+#define CACHE_MAX (KB_CACHE_MAX * 1024)
 
-#ifndef L3_CACHE
-#define L3_CACHE 6144
-#endif
+#define NITERATIONS 10
 
-#define TOTAL_CACHE (L1_CACHE+L2_CACHE+L3_CACHE)
+#define RTIME (100000)
 
 long long int microclock();
 
@@ -28,6 +23,23 @@ int main( int argc, char *argv[] )
 
   /* file handles */
   FILE *fp;
+
+  /* data arrays */
+  int x[CACHE_MAX];
+
+  /* loop variables */
+  int length;
+  int stride;
+  int idx;
+  int limit;
+  int l1steps, l2steps;
+  int i;
+  int y;
+  
+  /* timer variables */
+  long long int stime;
+  long long int l1time;
+  long long int l2time;
 
   /* read commandline arguments */
   if (argc == 1)
@@ -50,15 +62,67 @@ int main( int argc, char *argv[] )
   }
 
   /* print cache information */
-  fprintf(stdout, "L1 Cache : %d kB\n", L1_CACHE);
-  fprintf(stdout, "L2 Cache : %d kB\n", L2_CACHE);
-  fprintf(stdout, "L3 Cache : %d kB\n", L3_CACHE);
+//fprintf(stdout, "L1 Cache : %d kB\n", L1_CACHE);
+//fprintf(stdout, "L2 Cache : %d kB\n", L2_CACHE);
+//fprintf(stdout, "L3 Cache : %d kB\n", L3_CACHE);
 
   /* open output file and write header */
   fp = fopen(fname, "w");
   fprintf(stdout, "saving output to %s\n", fname);
   
   /* membench */
+  for (length = CACHE_MIN; length <= CACHE_MAX; length *= 2)
+  {
+    for (stride = 1; stride < length/2; stride *= 2)
+    {
+      limit = length - stride + 1;
+
+      l1steps = 0;
+      stime = microclock();
+      do
+      {
+        for (i = 1; i <= stride * NITERATIONS; i++)
+        {
+          for (idx = 0; idx < limit; idx += stride)
+          {
+//          printf("length = %d, stride = %d, idx = %d\n", length, stride, idx);
+            x[idx]++;
+          }
+        }
+
+        l1steps++;
+        l1time = microclock() - stime;
+
+      } while (l1time < RTIME);
+
+      l2steps = 0;
+      stime = microclock();
+      y = 0;
+      do
+      {
+        for (i = 1; i <= stride * NITERATIONS; i++)
+        {
+          for (idx = 0; idx < limit; idx += stride)
+          {
+//          printf("length = %d, stride = %d, idx = %d\n", length, stride, idx);
+            y += idx;
+          }
+        }
+
+        l2steps++;
+        l2time = microclock() - stime;
+
+      } while (l2steps < l1steps);
+
+      printf("length = %d, stride = %d\n", length, stride);
+
+      /* report timings */
+      double rtime = l1time - l2time;
+      double ns_per_step = 1000.0 * rtime / l1steps;
+      double reads_per_step = NITERATIONS * stride * ((limit-1.0)/stride+1.0);
+      fprintf(fp, "%ld, %ld, %f\n", length *sizeof(int), stride* sizeof(int), ns_per_step/reads_per_step);
+    }
+  }
 
   /* end of membench */
 
